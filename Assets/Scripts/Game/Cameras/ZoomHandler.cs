26ba05019279afdc97f3e156cccb05f18b82e00d
Callum Rose <46073440+callum-rose@users.noreply.github.com>
2021-03-16 19:58:38 +0000

    Added GUI kit

--- Assets/Scripts/Game/Cameras/ZoomHandler.cs
+++ Assets/Scripts/Game/Cameras/ZoomHandler.cs
@@ -6,50 +6,26 @@ namespace Game.Cameras
 {
 	internal class ZoomHandler : MonoBehaviour
 	{
-		[Serializable]
-		private struct CameraZoomData
-		{
-			[SerializeField] private CinemachineVirtualCamera camera;
-			[SerializeField, Min(0)] private float minFov;
-			[SerializeField, Min(0)] private float maxFov;
-
-			public CinemachineVirtualCamera Camera => camera;
-			public float MinFov => minFov;
-			public float MaxFov => maxFov;
-		}
-
-		[SerializeField] private CameraZoomData[] cameraZoomDatas;
+		[SerializeField] private CinemachineMixingCamera mixingCamera;
 		[SerializeField, Range(0, 1)] private float speedFactor = 1;
 		[SerializeField] private bool invertDirection;
+
+		private float SecondCamAmount => 1 - _firstCamAmount;
 		
-		private void OnEnable()
-		{
-			foreach (CameraZoomData cameraZoomData in cameraZoomDatas)
-			{
-				cameraZoomData.Camera.m_Lens.FieldOfView = cameraZoomData.MinFov;
-			}
-		}
-
+		private float _firstCamAmount = 1;
+		
 		public void OnPinchGesture(float pinchScale)
-		{
-			foreach (CameraZoomData cameraZoomData in cameraZoomDatas)
-			{
-				PinchCamera(cameraZoomData, pinchScale);
-			}
-		}
-
-		private void PinchCamera(CameraZoomData zoomData, float pinchScale)
-		{
-			float fov = zoomData.Camera.m_Lens.FieldOfView;
-
+		{			
 			if (invertDirection)
 			{
 				pinchScale = 1 / pinchScale;
 			}
-
-			fov *= Mathf.Pow(pinchScale, speedFactor);
-			fov = Mathf.Clamp(fov, zoomData.MinFov, zoomData.MaxFov);
-			zoomData.Camera.m_Lens.FieldOfView = fov;
+			
+			_firstCamAmount *= Mathf.Pow(pinchScale, speedFactor);
+			_firstCamAmount = Mathf.Clamp01(_firstCamAmount);
+			
+			mixingCamera.SetWeight(0, _firstCamAmount);
+			mixingCamera.SetWeight(1, SecondCamAmount);
 		}
 	}
 }
